1b890bd41609faf3d75a56dd50559d40ca19fe9e
Michael Fenwick <mike@mikefenwick.com>
2015-06-23 13:49:14 -0700

    Merge branch 'master' of C:\code\Dart\Color with conflicts.

--- lib/color_filter.dart
+++ lib/color_filter.dart
@@ -3,35 +3,75 @@ part of color;
 typedef Color FilterFunction(Color color, [List args]);
 
 class ColorFilter {
+
   final FilterFunction filterFunction;
   final Type baseType;
 
   ColorFilter(FilterFunction this.filterFunction, Type this.baseType);
 
+  /**
+   * Makes the color lighter by the percentage specified in the first argument, or by 10% if no percentage is specified. Percentages should be specified as a float, e.g. an argument of 0.25 will result in a color 25% lighter than the original. The lightening conversion is performed by adjusting the y component of the color in XYZ color space.
+   */
+  static ColorFilter lighten = new ColorFilter((XyzColor color, [List args]) {
+    num percent = 0.1;
+    if (args is List && args.length > 0 && args[0] is num) {
+      percent = args[0];
+    }
+    color.y *= 1 + percent;
+    return color.clone();
+  }, XyzColor);
+
+
+  /**
+   * Makes the color darker by the percentage specified in the first argument, or by 10% if no percentage is specified. Percentages should be specified as a float, e.g. an argument of 0.25 will result in a color 25% darker than the original. The darkening conversion is performed by adjusting the y component of the color in XYZ color space.
+   */
+  static ColorFilter darken = new ColorFilter((XyzColor color, [List args]) {
+    num percent = 0.1;
+    if (args is List && args.length > 0 && args[0] is num) {
+      percent = args[0];
+    }
+    color.y *= 1 - percent;
+  }, XyzColor);
+
+  /**
+   *
+   */
   static ColorFilter sepia = new ColorFilter((RgbColor color, [List args]) {
-    return new RgbColor(min(RgbColor.rMax,
-        (color.r * 0.393 + color.g * 0.769 + color.b * 0.189)), min(
-        RgbColor.gMax,
-        (color.r * 0.349 + color.g * 0.686 + color.b * 0.168)), min(
-        RgbColor.bMax, (color.r * 0.272 + color.g * 0.534 + color.b * 0.131)));
+    color = new RgbColor(
+      min(255, (color.r * 0.393 + color.g * 0.769 + color.b * 0.189)),
+      min(255, (color.r * 0.349 + color.g * 0.686 + color.b * 0.168)),
+      min(255, (color.r * 0.272 + color.g * 0.534 + color.b * 0.131))
+    );
   }, RgbColor);
 
+  /**
+   *
+   */
   static ColorFilter greyscale = new ColorFilter((RgbColor color, [List args]) {
     XyzColor xyz = color.toXyzColor();
-    return new RgbColor(RgbColor.rMax / 100 * xyz.y,
-        RgbColor.gMax / 100 * xyz.y, RgbColor.bMax / 100 * xyz.y);
+    color = new RgbColor(xyz.y, xyz.y, xyz.y);
   }, RgbColor);
 
-  static ColorFilter invert = new ColorFilter((RgbColor color, [List args]) {
-    return new RgbColor(RgbColor.rMax - color.r, RgbColor.gMax - color.g,
-        RgbColor.bMax - color.b);
-  }, RgbColor);
 
   Color call(Color color, List args) {
     Type startingColorSpace = color.runtimeType;
-    color = color._convert(this.baseType);
-    color = this.filterFunction(color, args);
-    color = color._convert(startingColorSpace);
+    color = _convert(color, this.baseType);
+    this.filterFunction(color, args);
+    color = _convert(color, startingColorSpace);
     return color;
   }
+
+  static Color _convert(Color color, Type colorType) {
+    switch (colorType) {
+      case RgbColor:
+        return color.toRgbColor();
+      case HslColor:
+        return color.toHslColor();
+      case XyzColor:
+        return color.toXyzColor();
+      case CielabColor:
+        return color.toCielabColor();
+    }
+  }
+
 }
